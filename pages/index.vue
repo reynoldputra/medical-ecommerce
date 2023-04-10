66508930daaf724fcc9c18f6e01a79630281de82
gunggus665 <gunggus665@gmail.com>
2023-04-10 23:47:05 +0700

    feat: Add UI Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-    <Tutorial />
-    <div class="text-5xl">Hello World</div>
+    <NavbarView />
+    <HomePageview />
   </div>
 </template>
 
 <script>
+import NavbarView from '@/components/NavbarView.vue'
+import HomePageview from '@/components/HomePageView.vue'
 export default {
   name: 'IndexPage',
+  components: {
+    NavbarView,
+    HomePageview,
+  },
 }
 </script>
