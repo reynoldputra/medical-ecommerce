c31d968b9d3b2acd84e49552d3b4b530a6202e86
ahnafms <ahnafmusyaffa47@gmail.com>
2023-04-10 22:23:07 +0700

    feat: add speech to text

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    <Tutorial />
     <div class="text-5xl">Hello World</div>
+    <button @click="startRecording">Start</button>
+    <button @click="stopRecording">Stop</button>
+    <p>{{transcript}}</p>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage',
+<script setup>
+import { ref } from 'vue'
+
+const isRecording = ref(false)
+const recognition = ref(null)
+const transcript = ref('')
+
+function startRecording() {
+  if (process.client) {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition
+    recognition.value = new SpeechRecognition()
+    recognition.value.start()
+    recognition.value.onresult = (event) => {
+      transcript.value = event.results[0][0].transcript
+    }
+    // Use the recognition object here
+  }
+}
+
+function stopRecording() {
+  isRecording.value = false
+  recognition.value.abort()
 }
 </script>
